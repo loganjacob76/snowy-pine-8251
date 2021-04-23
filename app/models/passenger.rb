de133721f6793b2b15b5818975dc8afd5231a1d1
loganjacob76 <nagolisback@gmail.com>
2021-04-23 10:03:34 -0600

    User story 3 AR method

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,11 @@
 class Passenger < ApplicationRecord
   has_many :passenger_flights, dependent: :destroy
   has_many :flights, through: :passenger_flights
+
+  def self.passengers_by_airline(airline)
+    joins(:flights)
+    .where('flights.airline_id = ?', airline.id)
+    .where('passengers.age >= 18')
+    .distinct
+  end
 end
